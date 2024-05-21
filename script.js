96f2e0abff62022669ae29bf640798771dd2c6e7
dorra chaffai <achref.chouikh@gmail.com>
2024-05-21 12:21:57 +0200

    fonction clearAll done

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
-const colorPickerBtn = document.querySelector("#color-picker")
+const colorPickerBtn = document.querySelector("#color-picker");
 const colorList = document.querySelector(".all-colors");
+const clearAll = document.querySelector(".clear-all");
 
+const copyColor = (elem) => {
+  navigator.clipboard.writeText(elem.dataset.color);
+  elem.innerText = "Copied!";
+  setTimeout(() => {
+    elem.innerText = elem.dataset.color;
+  }, 1000);
+};
 
-const pickedColors= JSON.parse(localStorage.getItem("picked-colors")) || []
-const showColors =()=>{
-   colorList.innerHTML = pickedColors
-     .map(
-       (color) => `
+const pickedColors = JSON.parse(localStorage.getItem("picked-colors")) || [];
+
+const showColors = () => {
+  if (!pickedColors.length) return;
+  colorList.innerHTML = pickedColors
+    .map(
+      (color) => `
                 <li class="color">
-                    <span class="rect" style="background: ${color}; border: 1px solid ${color== '#ffffff' ? '#ccc' : color}"></span>
-                    <span class="value">${color}</span>
+                    <span class="rect" style="background: ${color}; border: 1px solid ${
+        color == "#ffffff" ? "#ccc" : color
+      }"></span>
+                    <span class="value" data-color="${color}">${color}</span>
                 </li>
-    `
-     )
-     .join("");
-}
+            `
+    )
+    .join("");
+  document.querySelector(".picked-colors").classList.remove("hide");
+
+  document.querySelectorAll(".color").forEach((li) => {
+    li.addEventListener("click", (e) =>
+      copyColor(e.currentTarget.lastElementChild)
+    );
+  });
+};
+
 showColors();
-const activateEyeDropper = async() => {
-    try {
-        const eyeDropper= new EyeDropper()
-        const { sRGBHex } = await eyeDropper.open();
-        navigator.clipboard.writeText(sRGBHex);
-        if(!pickedColors.includes(sRGBHex)){
-              pickedColors.push(sRGBHex);
-              localStorage.setItem(
-                "picked-colors",
-                JSON.stringify(pickedColors)
-              );
-              showColors();
-        }
-      
-        console.log(pickedColors);
+
+const activateEyeDropper = async () => {
+  try {
+    const eyeDropper = new EyeDropper();
+    const { sRGBHex } = await eyeDropper.open();
+    navigator.clipboard.writeText(sRGBHex);
+    if (!pickedColors.includes(sRGBHex)) {
+      pickedColors.push(sRGBHex);
+      localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+      showColors();
     }
-    catch (error) {
-        console.error(error)
-    }
-}
-colorPickerBtn.addEventListener("click", activateEyeDropper)
+
+    console.log(pickedColors);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const clearAllColors = () => {
+  pickedColors.length = 0;
+  localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+  document.querySelector(".picked-colors").classList.add("hide");
+  colorList.innerHTML = ""; 
+  console.log("All colors cleared");
+};
+
+clearAll.addEventListener("click", clearAllColors);
+colorPickerBtn.addEventListener("click", activateEyeDropper);
